cfdecf13c05cbc3759f39117fc23a9b387269ec1
Lgsus <jisusdeivid@gmail.com>
2024-01-18 16:03:01 -0500

    punto 0

--- src/routes/signup.routes.js
+++ src/routes/signup.routes.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const db = require('../databases/db')
 
 const secretKey = 'abcdefghijk';
 
-router.post("/signup", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const cryptPass = await bcrypt.hash(password, 10)
-        const result = await db.signUp(email, cryptPass);
-        const user = {
-            id: result, // Supongamos que result contiene el ID del usuario registrado
-            email: email // Puedes agregar más información aquí si lo necesitas
-        };
+router.post('/signup', (req, res) => {
+    console.log(req.body); // Agrega esta línea para imprimir el cuerpo de la solicitud en la consola
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body['confirm-password'];
 
-        const token = jwt.sign(user, secretKey);
+    // Validaciones y lógica de registro aquí
 
-        res.status(201).json({ message: "User registered successfully", user_id: result, token: token });
-    } catch (error) {
-        res.status(500).json({ error: error.message || "An error occurred while registering user" });
-    }
+    // Respuesta simple para este ejemplo
+    res.send(`Registro exitoso para ${email}`);
 });
 
-module.exports = router
+
+module.exports = router;
